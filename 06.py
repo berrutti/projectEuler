c58e8883915ad8b697cc84eb9117d3e4a288fa67
Matias Berrutti <berrutti@gmail.com>
2018-11-20 21:31:58 -0300

    Sixth (finished)

--- 06.py
+++ 06.py
@@ -1,27 +1,23 @@
-primes = [1,2,3]
-number = 1
-counter = 3
-nth = 5
-
-def isPrime(someNumber):
+def isPrime(number, primes):
     for prime in primes:
-        if (prime > someNumber/2):
+        if (prime > number/2):
             return True
-        if (someNumber % prime == 0):
+        if (number % prime == 0):
             return False
     return True
 
-print('Calculating...')
-while True:
-    if (isPrime(number)):
-        primes.append(number)
-        counter = counter + 1
-        print('The number is:')
-        print(number)
+def findNthPrime(number):
+    primes = [2]
+    counter = 2
+    numberToCheck = 3
+    while (number >= counter):
+        if (isPrime(numberToCheck,primes)):
+            primes.append(numberToCheck)
+            counter = counter + 1
+        numberToCheck = numberToCheck + 2
+    return primes[-1]
+    
 
-    if (counter == nth):
-        print('The nth prime is: ')
-        print(number)
-        break
-    number+=2
-
+nth = int(input('Enter the nth prime you want to get: '))
+prime = findNthPrime(nth)
+print('The nth prime is: '+str(prime))
