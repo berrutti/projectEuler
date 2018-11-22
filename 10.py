ddef194f33dafaa1227317286adafb48d807946b
Matias Berrutti <berrutti@gmail.com>
2018-11-21 22:26:39 -0300

    Improvement, needs fixing

--- 10.py
+++ 10.py
@@ -1,22 +1,35 @@
-def isPrime(number, primes):
-    for prime in primes:
-        if (prime > number/2):
-            return True
-        if (number % prime == 0):
-            return False
-    return True
+class Pair(object):
+    def __init__(self, number, boolean):
+        self.number = number
+        self.isPrime = boolean
 
-def generatePrimes():
-    primes = [2]
-    numberToCheck = 3
-    while (numberToCheck < 2000000):
-        
-        if (isPrime(numberToCheck,primes)):
-            print('Found a prime:'+str(numberToCheck))
-            primes.append(numberToCheck)
-        numberToCheck = numberToCheck + 2
-    return primes
+def generateArray():
+    possiblePrimes = []
+    for number in range(2000000):
+        possiblePrimes.append(Pair(number,True))
+    return possiblePrimes
+
+def markMultiples(index,possiblePrimes):
+    jump = index
+    while (index < 2000000):
+        index = index + jump
+        possiblePrimes[index].isPrime = False
+
+sum = 0    
+possiblePrimes = generateArray()
+possiblePrimes[0].isPrime = False
+possiblePrimes[1].isPrime = False
+
+for index in range(2000000):
+    if (possiblePrimes[index].isPrime):
+        sum = sum + possiblePrimes[index].number
+        markMultiples(index,possiblePrimes)
+
+#Remove 0 and 1 which are not primes
+possiblePrimes.pop(0)
+possiblePrimes.pop(0)
+
+
+print('The sum of all primes below 2000000 is: ')
+print(sum)
     
-primes = generatePrimes()
-print('The primes below two million are: ')
-print(primes)
