fddcdbddcb9694d49a8f532e2c19674844a7dbd8
Matias Berrutti <berrutti@gmail.com>
2019-05-06 20:21:36 -0300

    Solved 10, better algorithm

--- 10.py
+++ 10.py
@@ -1,35 +1,14 @@
-class Pair(object):
-    def __init__(self, number, boolean):
-        self.number = number
-        self.isPrime = boolean
+def markNonPrimes(possiblePrime,total,sieve):
+    for i in range(possiblePrime*possiblePrime, total, possiblePrime):
+        sieve[i] = False
 
-def generateArray():
-    possiblePrimes = []
-    for number in range(2000000):
-        possiblePrimes.append(Pair(number,True))
-    return possiblePrimes
+def sumPrimes(total):
+    sum = 0
+    sieve = [True] * total
+    for possiblePrime in range(2, total):
+        if sieve[possiblePrime]:
+            sum += possiblePrime
+            markNonPrimes(possiblePrime,total,sieve)
+    return sum
 
-def markMultiples(index,possiblePrimes):
-    jump = index
-    while (index < 2000000):
-        index = index + jump
-        possiblePrimes[index].isPrime = False
-
-sum = 0    
-possiblePrimes = generateArray()
-possiblePrimes[0].isPrime = False
-possiblePrimes[1].isPrime = False
-
-for index in range(2000000):
-    if (possiblePrimes[index].isPrime):
-        sum = sum + possiblePrimes[index].number
-        markMultiples(index,possiblePrimes)
-
-#Remove 0 and 1 which are not primes
-possiblePrimes.pop(0)
-possiblePrimes.pop(0)
-
-
-print('The sum of all primes below 2000000 is: ')
-print(sum)
-    
+print(sumPrimes(2000000))
